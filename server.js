6a0346f768e83ffbe0778cfdd12839891cab84d2
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-04 12:22:14 -0200

    Creating route to recover all todos

--- server.js
+++ server.js
@@ -2,8 +2,30 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id : 1,
+    description : 'Meet mom for lunch',
+    completed : false
+}, {
+    id : 2,
+    description : 'Go to market',
+    completed : false
+}, {
+    id : 3,
+    description : 'Feed the cat',
+    completed : true
+}];
+
 app.get('/', (req, res) => {
     res.send('Todo API Root');
+});
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+
 });
 
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
