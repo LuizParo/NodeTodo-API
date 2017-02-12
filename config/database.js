cbbd3f3fa5dda531047cf98d854b23571eb0f65b
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-12 17:13:34 -0200

    Configuring database connection to work on heroku or locally depending of the environment variables

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 'use strict';
 
 let Sequelize = require('sequelize');
+const ENV = process.env.NODE_ENV || 'development';
 
-let sequelize = new Sequelize(undefined, undefined, undefined, {
-    dialect : 'sqlite',
-    storage : `${__dirname}/dev-todo-api.sqlite`
-});
+let sequelize = ENV === 'development'
+    ? new Sequelize(undefined, undefined, undefined, {
+        dialect : 'sqlite',
+        storage : `${__dirname}/dev-todo-api.sqlite`
+    })
+    : new Sequelize(process.env.DATABASE_URL, {
+        dialect : 'postgres'
+    });
 
 module.exports = sequelize;
