99b85684a37a12816996720321eb9c1d0421cb8a
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-12 16:24:20 -0200

    Refactoring TodoService and TodoApi to use the es6 class definition

--- app/todo/api/TodoApi.js
+++ app/todo/api/TodoApi.js
@@ -1,69 +1,67 @@
 'use strict';
 
-let _ = require('underscore');
+class TodoApi {
 
-function TodoApi(service) {
-    this._service = service;
-};
+    constructor(service) {
+        this._service = service;
+    }
 
-TodoApi.prototype.info = function(req, res) {
-    res.send('Todo API Root');
-};
+    info(req, res) {
+        res.send('Todo API Root');
+    }
 
-TodoApi.prototype.list = function(req, res) {
-    this._service.list(req.query)
-        .then(function(todos) {
-            res.json(todos);
-        })
-        .catch(function(error) {
-            res.status(error.status).json(error);
-        });
-};
+    list(req, res) {
+        this._service.list(req.query)
+            .then(function(todos) {
+                res.json(todos);
+            })
+            .catch(function(error) {
+                res.status(error.status).json(error);
+            });
+    }
 
-TodoApi.prototype.findById = function(req, res) {
-    this._service.findById(req.params.id)
-        .then(todo => {
-            res.json(todo);
-        })
-        .catch(error => {
-            res.status(error.status).json(error);
-        });
-};
+    findById(req, res) {
+        this._service.findById(req.params.id)
+            .then(todo => {
+                res.json(todo);
+            })
+            .catch(error => {
+                res.status(error.status).json(error);
+            });
+    }
 
-TodoApi.prototype.save = function(req, res) {
-    let body = req.body;
+    save(req, res) {
+        let body = req.body;
 
-    this._service.save(body)
-        .then(function(todoId) {
-            res.location(`${req.get('host')}/todos/${todoId}`);
-            res.sendStatus(201);
-        })
-        .catch(function(error) {
-            res.status(error.status).json(error);
-        });
+        this._service.save(body)
+            .then(function(todoId) {
+                res.location(`${req.get('host')}/todos/${todoId}`);
+                res.sendStatus(201);
+            })
+            .catch(function(error) {
+                res.status(error.status).json(error);
+            });
+    }
 
-};
+    update(req, res) {
+        this._service.update(req.params.id, req.body)
+            .then(function() {
+                res.sendStatus(204);
+            })
+            .catch(function(error) {
+                res.status(error.status).json(error);
+            });
+    }
 
-TodoApi.prototype.update = function(req, res) {
-    this._service.update(req.params.id, req.body)
-        .then(function() {
-            res.sendStatus(204);
-        })
-        .catch(function(error) {
-            res.status(error.status).json(error);
-        });
-};
+    delete(req, res) {
+        this._service.delete(req.params.id)
+            .then(function() {
+                res.sendStatus(204);
+            })
+            .catch(function(error) {
+                res.status(error.status).json(error);
+            });
+    }
+}
 
-TodoApi.prototype.delete = function(req, res) {
-    this._service.delete(req.params.id)
-        .then(function() {
-            res.sendStatus(204);
-        })
-        .catch(function(error) {
-            res.status(error.status).json(error);
-        });
-};
-
-module.exports = function() {
-    return TodoApi;
-};
+module.exports = () => TodoApi;
--- app/todo/services/TodoService.js
+++ app/todo/services/TodoService.js
@@ -6,108 +6,109 @@ let InvalidTodoException = require('../exceptions/InvalidTodoException');
 
 let todos = [];
 
-function TodoService(validator, repository) {
-    this._validator = validator;
-    this._repository = repository;
-};
+class TodoService {
 
-TodoService.prototype.list = function(filters) {
-    return new Promise(function(resolve, reject) {
-        try {
-            let filteredTodos = todos;
+    constructor(validator, repository) {
+        this._validator = validator;
+        this._repository = repository;
+    }
 
-            if(filters.hasOwnProperty('completed')) {
-                filteredTodos = _.where(todos, {completed : filters.completed === 'true'});
+    list(filters) {
+        return new Promise(function(resolve, reject) {
+            try {
+                let filteredTodos = todos;
+
+                if(filters.hasOwnProperty('completed')) {
+                    filteredTodos = _.where(todos, {completed : filters.completed === 'true'});
+                }
+
+                if(filters.hasOwnProperty('q') && filters.q.length) {
+                    filteredTodos = _.filter(filteredTodos, (todo) => todo.description
+                        .toLowerCase()
+                        .indexOf(filters.q.toLowerCase()) > -1);
+                }
+
+                resolve(filteredTodos);
+            } catch (e) {
+                console.log(e);
+                reject({
+                    status : e.status || 500,
+                    message : e.message
+                });
             }
+        });
+    }
 
-            if(filters.hasOwnProperty('q') && filters.q.length) {
-                filteredTodos = _.filter(filteredTodos, (todo) => todo.description
-                    .toLowerCase()
-                    .indexOf(filters.q.toLowerCase()) > -1);
+    findById(id) {
+        return this._repository.findById(id)
+            .then(todo => this._validator.assertIfExists(todo, id))
+            .catch(error => this._handleError(error));
+    }
+
+    save(todoDTO) {
+        let todo = {
+            description : todoDTO.description,
+            completed : todoDTO.completed
+        };
+
+        return this._repository.save(todo)
+            .catch(error => this._handleError(error));
+    }
+
+    update(id, todoDTO) {
+        return new Promise(function(resolve, reject) {
+            try {
+                let todo = _.findWhere(todos, {id : parseInt(id, 10)});
+
+                if(!todo) {
+                    throw new TodoNotFoundException(`Todo with id ${id} not found!`);
+                }
+
+                if(!_.isBoolean(todoDTO.completed) || !_.isString(todoDTO.description) || !todoDTO.description.length) {
+                    throw new InvalidTodoException(`Todo must have a valid 'description' and 'completed' status!`);
+                }
+
+                _.extend(todo, {description : todoDTO.description, completed : todoDTO.completed});
+                resolve();
+            } catch (e) {
+                console.log(e);
+                reject({
+                    status : e.status || 500,
+                    message : e.message
+                });
             }
+        });
+    }
 
-            resolve(filteredTodos);
-        } catch (e) {
-            console.log(e);
-            reject({
-                status : e.status || 500,
-                message : e.message
-            });
-        }
-    });
-};
+    delete(id) {
+        return new Promise(function(resolve, reject){
+            try {
+                let todo = _.findWhere(todos, {id : parseInt(id, 10)});
 
-TodoService.prototype.findById = function(id) {
-    return this._repository.findById(id)
-        .then(todo => this._validator.assertIfExists(todo, id))
-        .catch(error => _handleError(error));
-};
+                if(!todo) {
+                    throw new TodoNotFoundException(`Todo with id ${id} not found!`);
+                }
 
-TodoService.prototype.save = function(todoDTO) {
-    let todo = {
-        description : todoDTO.description,
-        completed : todoDTO.completed
-    };
-
-    return this._repository.save(todo)
-        .catch(error => _handleError(error));
-};
-
-TodoService.prototype.update = function(id, todoDTO) {
-    return new Promise(function(resolve, reject) {
-        try {
-            let todo = _.findWhere(todos, {id : parseInt(id, 10)});
-
-            if(!todo) {
-                throw new TodoNotFoundException(`Todo with id ${id} not found!`);
+                todos = _.without(todos, todo);
+                resolve();
+            } catch (e) {
+                console.log(e);
+                reject({
+                    status : e.status || 500,
+                    message : e.message
+                });
             }
+        });
+    }
 
-            if(!_.isBoolean(todoDTO.completed) || !_.isString(todoDTO.description) || !todoDTO.description.length) {
-                throw new InvalidTodoException(`Todo must have a valid 'description' and 'completed' status!`);
-            }
+    _handleError(error) {
+        console.log(error);
 
-            _.extend(todo, {description : todoDTO.description, completed : todoDTO.completed});
-            resolve();
-        } catch (e) {
-            console.log(e);
-            reject({
-                status : e.status || 500,
-                message : e.message
-            });
-        }
-    });
-};
-
-function _handleError(error) {
-    console.log(error);
-
-    return {
-        status : error.status || 500,
-        message : error.message
-    };
+        return {
+            status : error.status || 500,
+            message : error.message
+        };
+    }
 }
 
-TodoService.prototype.delete = function(id) {
-    return new Promise(function(resolve, reject){
-        try {
-            let todo = _.findWhere(todos, {id : parseInt(id, 10)});
-
-            if(!todo) {
-                throw new TodoNotFoundException(`Todo with id ${id} not found!`);
-            }
-
-            todos = _.without(todos, todo);
-            resolve();
-        } catch (e) {
-            console.log(e);
-            reject({
-                status : e.status || 500,
-                message : e.message
-            });
-        }
-    });
-};
-
-module.exports = function() {
-    return TodoService;
-};
+module.exports = () => TodoService;
